4409473baef31efe0b5561f8c2246dcfb632fdaf
Victoria Khoreva <victheliar@gmail.com>
2025-11-09 16:41:46 +0200

    pylint exercise

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -40,25 +40,25 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
-        
+
     def test_negatiivinen_tilavuus(self):
         self.assertAlmostEqual(self.nega_varasto.tilavuus, 0)
-        
+
     def test_negatiivinen_saldo(self):
         self.assertAlmostEqual(self.nega_saldo_varasto.saldo, 0)
 
     def test_negatiivinen_maara(self):
         self.assertAlmostEqual(self.varasto.lisaa_varastoon(-5), None)
-    
+
     def test_liian_suuri_maara(self):
         self.varasto.lisaa_varastoon(50)
         self.assertAlmostEqual(self.varasto.saldo, 10)
-        
+
     def test_negatiivinen_otto(self):
         self.assertAlmostEqual(self.varasto.ota_varastosta(-5), 0)
-        
+
     def test_kaikki_mita_voidaan(self):
         self.assertAlmostEqual(self.varasto.ota_varastosta(50), 0)
-    
+
     def test_print(self):
         self.assertAlmostEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
